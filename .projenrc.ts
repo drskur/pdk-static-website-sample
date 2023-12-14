47c5020dc6e5e40fa9b15d9ab775fa73c39e8b48
ChangHun Yoon <drskur@amazon.com>
2023-12-14 10:47:25 +0900

    feat: add infra package

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,6 +1,7 @@
 import { MonorepoTsProject } from "@aws/pdk/monorepo";
 import { NodePackageManager } from "projen/lib/javascript";
 import { NextJsTypeScriptProject } from "projen/lib/web";
+import { InfrastructureTsProject } from "@aws/pdk/infrastructure";
 
 const project = new MonorepoTsProject({
   devDeps: ["@aws/pdk"],
@@ -21,5 +22,16 @@ const webapp = new NextJsTypeScriptProject({
   tailwind: false,
 });
 webapp.addGitIgnore("out");
+
+const infra = new InfrastructureTsProject({
+  parent: project,
+  name: "infra",
+  outdir: "packages/infra",
+  packageManager: NodePackageManager.PNPM,
+  prettier: true,
+  eslint: true,
+});
+
+project.addImplicitDependency(infra, webapp);
 
 project.synth();
--- README.md
+++ README.md
@@ -10,3 +10,14 @@ npm i -g pnpm
 ```
 pnpm add -g aws-cdk
 ```
+- cdk bootstrap
+```
+cd packages/infra
+# 다음 명령의 실행을 위해, 리소스를 배포할 AWS 계정의 cli credential 설정을 해야 한다. (ex> aws configure 등의 방법)
+cdk bootstrap
+```
+
+## packages
+- CDK IaC (packages/infra)
+- 웹앱 (packages/webapp)
+
